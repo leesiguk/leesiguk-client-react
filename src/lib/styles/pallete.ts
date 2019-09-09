ae98056bbc4383ebae3de99e1fc29ae54016e1fb
youngjin <masichall@gmail.com>
2019-09-09 23:35:31 +0900

    blue color 추가

--- src/lib/styles/pallete.ts
+++ src/lib/styles/pallete.ts
@@ -10,6 +10,18 @@ const palette = {
   gray7: '#495057',
   gray8: '#343A40',
   gray9: '#212529',
+
+  /* blue */
+  blue0: '#e7f5ff',
+  blue1: '#d0ebff',
+  blue2: '#a5d8ff',
+  blue3: '#74c0fc',
+  blue4: '#4dabf7',
+  blue5: '#339af0',
+  blue6: '#228be6',
+  blue7: '#1c7ed6',
+  blue8: '#1971c2',
+  blue9: '#1864ab',
 };
 
 export default palette;
