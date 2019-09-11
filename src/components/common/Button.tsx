53dfc21e5d944bf4b8151e458edb8903f18811c5
youngjin <masichall@gmail.com>
2019-09-11 17:37:05 +0900

    Create Button.tsx

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+import palette, { buttonColorMap } from '../../lib/styles/pallete';
+
+type ColorType = 'teal' | 'gray' | 'darkGray' | 'lightGray';
+type ButtonSize = 'medium' | 'large';
+
+const ButtonBlock = styled.button<{
+  color: ColorType;
+  inline: boolean;
+  size: ButtonSize;
+  link: boolean;
+}>`
+  display: inline-flex;
+  align-items: center;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  color: white;
+  color: ${props => buttonColorMap[props.color].color};  
+  border-radius: 4px;
+  padding-top: 0;
+  padding-bottom: 0;
+  ${props =>
+    props.link
+      ? css`
+          background-color: transparent;
+          &:hover,
+          &:focus {
+            background-color: rgba(255, 255, 255, 0.16);
+          }
+        `
+      : css`
+          background: ${buttonColorMap[props.color].backgroundColor};
+          &:hover,
+          &:focus {
+            background-color: ${buttonColorMap[props.color]
+              .hoverBackgroundColor};
+          }
+        `}
+  ${props =>
+    props.inline &&
+    css`
+      & + & {
+        margin-left: 0.5rem;
+      }
+    `}
+  ${props =>
+    props.size === 'medium' &&
+    css`
+      height: 2rem;
+      padding-left: 1.25rem;
+      padding-right: 1.25rem;
+      font-size: 1rem;
+    `}
+    ${props =>
+      props.size === 'large' &&
+      css`
+        height: 2.5rem;
+        padding-left: 1.125rem;
+        padding-right: 1.125rem;
+        & + & {
+          margin-left: 0.875rem;
+        }
+        font-size: 1.125rem;
+      `}
+    &:disabled {
+      cursor: not-allowed;
+      background: ${palette.gray3};
+      color: ${palette.gray5};
+      &:hover {
+        background: ${palette.gray3};
+        color: ${palette.gray5};
+      }
+    }
+`;
+
+interface ButtonProps extends Omit<React.HTMLProps<HTMLButtonElement>, 'size'> {
+  color?: ColorType;
+  inline?: boolean;
+  size?: ButtonSize;
+  link?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  ref,
+  color = 'gray',
+  inline,
+  size = 'medium',
+  link,
+  ...rest
+}) => {
+  const htmlProps = rest as any;
+  return (
+    <ButtonBlock
+      color={color}
+      inline={inline}
+      size={size}
+      link={link}
+      {...htmlProps}
+      onClick={e => {
+        if (htmlProps.onClick) {
+          htmlProps.onClick(e);
+        }
+        (e.target as HTMLButtonElement).blur();
+      }}
+    >
+      {children}
+    </ButtonBlock>
+  );
+};
+
+export default Button;
--- src/lib/styles/pallete.ts
+++ src/lib/styles/pallete.ts
@@ -22,6 +22,42 @@ const palette = {
   blue7: '#1c7ed6',
   blue8: '#1971c2',
   blue9: '#1864ab',
+
+  /* red */
+  red0: '#fff5f5',
+  red1: '#ffe3e3',
+  red2: '#ffc9c9',
+  red3: '#ffa8a8',
+  red4: '#ff8787',
+  red5: '#ff6b6b',
+  red6: '#fa5252',
+  red7: '#f03e3e',
+  red8: '#e03131',
+  red9: '#c92a2a'
+};
+
+export const buttonColorMap: {
+  [color: string]: {
+    backgroundColor: string;
+    color: string;
+    hoverBackgroundColor: string;
+  };
+} = {
+  lightGray: {
+    backgroundColor: palette.gray2,
+    color: palette.gray7,
+    hoverBackgroundColor: palette.gray1
+  },
+  gray: {
+    backgroundColor: palette.gray6,
+    color: 'white',
+    hoverBackgroundColor: palette.gray5
+  },
+  darkGray: {
+    backgroundColor: palette.gray8,
+    color: 'white',
+    hoverBackgroundColor: palette.gray6
+  }
 };
 
 export default palette;
