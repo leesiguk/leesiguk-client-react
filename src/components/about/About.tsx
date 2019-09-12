6ce24b856dc1009bdbb0171b78603b024c4a65f7
youngjin <masichall@gmail.com>
2019-09-12 21:03:53 +0900

    about page 디자인 및 기능 개선

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import point1 from '../../assets/point_01.png';
 import point2 from '../../assets/point_02.png';
@@ -10,14 +10,12 @@ import palette from '../../lib/styles/pallete';
 
 const AboutBlock = styled.div`
   background-color: #282d49;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 10;
+`;
+
+const MainContent = styled.div`
+  position: relative;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
 
@@ -57,19 +55,58 @@ const HomeLink = styled(Link)`
   }
 `;
 
-const Footer = styled.div`
+const ArrowDown = styled.div<{ scrollY: number }>`
   position: absolute;
-  top: 0;
-  left: 0;
   bottom: 0;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 16px 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+
+  i {
+    font-size: 48px;
+    transition: opacity 0.3s;
+    color: rgba(255, 255, 255, 0.26);
+    animation-name: bounce;
+    animation-duration: 2.5s;
+    animation-fill-mode: both;
+    animation-iteration-count: infinite;
+
+    ${props =>
+      props.scrollY > 0 &&
+      css`
+        opacity: 0;
+      `}
+  }
+
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-15px);
+    }
+    60% {
+      transform: translateY(-7px);
+    }
+  }
 `;
 
-const FooterButtonLink = styled.a`
+const Menu = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 48px;
+  flex-wrap: wrap;
+`;
+
+const ButtonLink = styled.a`
   text-decoration: none;
   i {
     margin-right: 8px;
@@ -80,20 +117,55 @@ const FooterButtonLink = styled.a`
   }
 `;
 
+const ButtonRouterLink = styled(Link)`
+  text-decoration: none;
+  i {
+    margin-right: 8px;
+  }
+`;
+
 interface AboutProps {}
 
 const MainCard: React.FC<AboutProps> = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const onScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    setScrollY(window.scrollY);
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
     <AboutBlock>
-      <HomeLink to="/">
-        <img src="/img/icons/social-810x810.png" alt="logo" className="logo" />
-      </HomeLink>
-      <img className="point geon" src={point1} alt="geon" />
-      <img className="point gon" src={point2} alt="gon" />
-      <img className="point gam" src={point3} alt="gam" />
-      <img className="point li" src={point4} alt="li" />
-      <Footer>
-        <FooterButtonLink
+      <MainContent>
+        <HomeLink to="/">
+          <img
+            src="/img/icons/social-810x810.png"
+            alt="logo"
+            className="logo"
+          />
+        </HomeLink>
+        <img className="point geon" src={point1} alt="geon" />
+        <img className="point gon" src={point2} alt="gon" />
+        <img className="point gam" src={point3} alt="gam" />
+        <img className="point li" src={point4} alt="li" />
+      </MainContent>
+      <ArrowDown scrollY={scrollY}>
+        <i className="material-icons">keyboard_arrow_down</i>
+      </ArrowDown>
+      <Menu>
+        <ButtonRouterLink to="/">
+          <Button link>
+            <i className="material-icons">home</i>
+            메인화면
+          </Button>
+        </ButtonRouterLink>
+        <ButtonLink
           href="mailto:siguklee@gamil.com"
           target="_blank"
           rel="noopener noreferrer"
@@ -102,8 +174,8 @@ const MainCard: React.FC<AboutProps> = () => {
             <i className="material-icons">email</i>
             문의하기
           </Button>
-        </FooterButtonLink>
-        <FooterButtonLink
+        </ButtonLink>
+        <ButtonLink
           href="https://github.com/leesiguk"
           target="_blank"
           rel="noopener noreferrer"
@@ -112,24 +184,24 @@ const MainCard: React.FC<AboutProps> = () => {
             <i className="material-icons">code</i>
             Github
           </Button>
-        </FooterButtonLink>
-        <FooterButtonLink href="/privacy">
+        </ButtonLink>
+        <ButtonRouterLink to="/privacy">
           <Button link>
             <i className="material-icons">policy</i>
             개인정보 처리방침
           </Button>
-        </FooterButtonLink>
-        <FooterButtonLink
+        </ButtonRouterLink>
+        <ButtonLink
           href="https://www.buymeacoffee.com/f7ue0MvSn"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Button link>
             <i className="material-icons heart">favorite</i>
-            커피 한 잔 지원
+            후원
           </Button>
-        </FooterButtonLink>
-      </Footer>
+        </ButtonLink>
+      </Menu>
     </AboutBlock>
   );
 };
