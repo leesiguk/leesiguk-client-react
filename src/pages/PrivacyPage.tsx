689ab5306c8555dcc1c29d9aa111c0463b0d799c
youngjin <masichall@gmail.com>
2019-09-12 16:53:02 +0900

    privacy page 초안 구현

--- src/pages/PrivacyPage.tsx
+++ src/pages/PrivacyPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import Privacy from '../components/privacy/Privacy';
+import AppBar from '../components/common/AppBar';
+
+const PrivacyPageBlock = styled.div`
+  background-color: #282d49;
+  width: 100%;
+  height: 100%;
+`;
 
 const PrivacyPage: React.FC = () => {
-  return <div>privacy</div>;
+  return (
+    <PrivacyPageBlock>
+      <AppBar />
+      <Privacy />
+    </PrivacyPageBlock>
+  );
 };
 
 export default PrivacyPage;
