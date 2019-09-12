9047396b0467ba72f00d9140d61f08450c60720b
youngjin <masichall@gmail.com>
2019-09-13 00:21:39 +0900

    pwa 설정 진행

--- src/index.tsx
+++ src/index.tsx
@@ -25,4 +25,4 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
