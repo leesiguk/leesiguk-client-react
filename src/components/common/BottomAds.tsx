045850ca653787c1b4726e839312be15848b1be9
youngjin <masichall@gmail.com>
2019-09-20 23:29:13 +0900

    css import 제거

--- src/components/common/BottomAds.tsx
+++ src/components/common/BottomAds.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { breakpoints } from '../../lib/styles/responsive';
 
 const BottomAdsBlock = styled.div`
